246951be69d416eb89174f3e7038a4434a6b5187
melisand <melisandoval@live.com.ar>
2022-12-08 23:58:02 +0100

    add iteration 3

--- lab/src/components/MessageCard.vue
+++ lab/src/components/MessageCard.vue
@@ -1,7 +1,12 @@
 <template>
   <p>{{ message }}</p>
+  <button @click="helloWorld">Click me and see the console</button>
 </template>
 
 <script setup>
 const message = "This is a paragraph inside the MessageCard component";
+
+function helloWorld() {
+  console.log("Hello World");
+}
 </script>
